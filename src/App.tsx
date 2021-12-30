b43f7d9d756bbd07b4ee3d28f3bfca64dc5fcad4
Pawel <inne.poczta@gmail.com>
2021-12-30 20:20:21 +0100

    Added zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { z } from "zod";
+
+const foo = z.string();
 
 function App() {
   return (
